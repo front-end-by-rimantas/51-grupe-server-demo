3e62db60a9d2e0b5b1a6aa721fbe055646e19142
Rimantas <belauzas@gmail.com>
2024-09-19 09:43:17 +0300

    Up

--- index.js
+++ index.js
@@ -15,8 +15,21 @@ app.get('/register', (req, res) => {
     return res.send('Register page');
 });
 
+app.get('/secret', (req, res) => {
+    return res.status(401).send('Secret page');
+});
+
 app.get('*', (req, res) => {
     return res.send('404 - page not found');
+});
+
+app.use((req, res, next) => {
+    return res.status(404).send("Sorry can't find that!");
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    return res.status(500).send('Something broke!');
 });
 
 app.listen(port, () => {
